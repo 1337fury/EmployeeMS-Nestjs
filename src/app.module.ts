65c346276e3babc53fb8fca22d0ae1f05269dbe5
1337fury <elouardyabdelhalim@gmail.com>
2024-10-26 20:59:54 +0100

    set up AppModule with global configuration and MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    EmployeeModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
