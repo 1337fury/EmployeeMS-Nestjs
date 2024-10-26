678475b5535b54d21e0bd3cff3059c9c43c62893
1337fury <elouardyabdelhalim@gmail.com>
2024-10-26 20:54:11 +0100

    Add employee dto

---
+++ src/employee/dto/create-employee.dto.ts
@@ -0,0 +1,27 @@
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreateEmployeeDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsPhoneNumber('MA')
+  @IsNotEmpty()
+  phoneNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  position: string;
+
+  @IsString()
+  @IsNotEmpty()
+  department: string;
+}
