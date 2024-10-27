6cf88ac01096b08529de55f7a8ae55acbdacf646
1337fury <elouardyabdelhalim@gmail.com>
2024-10-27 04:06:31 +0100

    Start documenting api

--- src/employee/dto/create-employee.dto.ts
+++ src/employee/dto/create-employee.dto.ts
@@ -1,31 +1,69 @@
-import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString, IsIn } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../../auth/roles.enum'; 
 
 export class CreateEmployeeDto {
+  @ApiProperty({
+	description: 'First name of the employee',
+	example: 'Alice',
+  })
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty({
+    description: 'Last name of the employee',
+    example: 'Admin',
+  })
   @IsString()
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty({
+    description: 'Unique email address of the employee',
+    example: 'alice.admin@example.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Phone number of the employee',
+    example: '+212637278350',
+  })
   @IsPhoneNumber('MA')
   @IsNotEmpty()
   phoneNumber: string;
 
+  @ApiProperty({
+    description: 'Position of the employee',
+    example: 'Administrator',
+  })
   @IsString()
   @IsNotEmpty()
   position: string;
 
+  @ApiProperty({
+    description: 'Department of the employee',
+    example: 'Administration',
+  })
   @IsString()
   @IsNotEmpty()
   department: string;
 
+  @ApiProperty({
+	description: 'Password of the employee',
+	example: 'password',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
+
+  @ApiProperty({
+    description: 'Role of the employee',
+    example: 'admin',
+    enum: ['admin', 'manager', 'employee'],
+  })
+  @IsIn(['admin', 'manager', 'employee'])
+  role: Role;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,31 @@ async function bootstrap() {
 	forbidNonWhitelisted: true,
 	transform: true,
   }));
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Employee Management System API')
+    .setDescription('Comprehensive API documentation for the Employee Management System')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Enter JWT token as: Bearer <token>',
+        in: 'header',
+      },
+      'JWT',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
 
   await app.listen(port);
