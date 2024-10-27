b7ccc9f7ec866a59325fbff0a8cea83b8435b5d7
1337fury <elouardyabdelhalim@gmail.com>
2024-10-27 01:32:19 +0100

    Import AuthModule to access JwtAuthGuard and RolesGuard

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { EmployeeController } from './employee.controller';
 import { EmployeeService } from './employee.service';
 import { Employee, EmployeeSchema } from './schemas/employee.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Employee.name, schema: EmployeeSchema }]),
+	AuthModule,
   ],
   controllers: [EmployeeController],
   providers: [EmployeeService],
