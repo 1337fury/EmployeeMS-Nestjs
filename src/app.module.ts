f29fcdf9bfb3c58ff02be71df64a8522fe16c8d5
1337fury <elouardyabdelhalim@gmail.com>
2024-10-27 01:34:44 +0100

    register RolesGuard globally so it applies to all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EmployeeModule } from './employee/employee.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './auth/roles.guard';
 
 @Module({
   imports: [
@@ -20,6 +22,11 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+	{
+		provide: APP_GUARD,
+		useClass: RolesGuard,
+	},
+  ],
 })
 export class AppModule {}
